e9c1c5a5c988545abaf48f478365fb6be43b9fad
Natemily <nerezko.n@gmail.com>
2022-01-19 09:54:56 +0300

    5.7. Ипотека [#156315 #252350]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,12 +3,9 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
-        for (int i = 0; i < 100; i++) {
+        while (amount > 0) {
             amount = amount + amount * percent / 100 - salary;
             year++;
-            if (amount <= 0) {
-                break;
-            }
         }
         return year;
     }
