02aa635623cb6af911aadf8feeafc63c57e99f18
Natemily <nerezko.n@gmail.com>
2022-02-02 10:57:07 +0300

    3. Перегрузить метод max для трех чисел [#122644 #258788]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -2,20 +2,19 @@ package ru.job4j.condition;
 
 public class Max {
 
-    public int maximum(int first, int second) {
+    public static int maximum(int first, int second) {
         return first > second ? first : second;
     }
 
-    public int maximum(int first, int second, int third) {
+    public static int maximum(int first, int second, int third) {
         return maximum(third, maximum(first, second));
     }
 
-    public int maximum(int first, int second, int third, int fourth) {
+    public static int maximum(int first, int second, int third, int fourth) {
         return maximum(maximum(first, second), maximum(third, fourth));
     }
 
     public static void main(String[] args) {
-        Max max = new Max();
-        System.out.println(max.maximum(5, 7, 9, 12));
+        System.out.println(maximum(5, 7, 9, 12));
     }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -5,29 +5,42 @@ import org.junit.Test;
 import org.junit.Assert;
 import ru.job4j.condition.Max;
 
+import static ru.job4j.condition.Max.maximum;
+
 public class MaxTest {
 
     @Test
     public void whenMax1to2then2() {
-        Max max = new Max();
-        int y = max.maximum(2, 1);
+        int y = maximum(2, 1);
         int expected = 2;
         Assert.assertEquals(expected, y);
     }
 
     @Test
     public void whenMax3to2then3() {
-        Max max = new Max();
-        int y = max.maximum(2, 3);
+        int y = maximum(2, 3);
         int expected = 3;
         Assert.assertEquals(expected, y);
     }
 
     @Test
     public void whenMax2to2then2() {
-        Max max = new Max();
-        int y = max.maximum(2, 2);
+        int y = maximum(2, 2);
         int expected = 2;
+        Assert.assertEquals(expected, y);
+    }
+
+    @Test
+    public void whenMax2and3and4then4() {
+        int y = maximum(2, 3, 4);
+        int expected = 4;
+        Assert.assertEquals(expected, y);
+    }
+
+    @Test
+    public void whenMax7and2and5and15then15() {
+        int y = maximum(7, 2, 5, 15);
+        int expected = 15;
         Assert.assertEquals(expected, y);
     }
 }
